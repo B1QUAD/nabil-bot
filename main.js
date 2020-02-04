8b47dba9602fc79918b4a9ede5ec0fd6927d7302
Anubhav <44627486+sirdonutsiiv@users.noreply.github.com>
2020-02-04 09:03:40 -0600

    Update main.js

--- main.js
+++ main.js
@@ -134,9 +134,9 @@ bot.on('message', message => {
         message.channel.send("Yeet status:\nAnubhav: " + yeetCount["Anubhav"] + " yeets\nNabil: " + yeetCount["Nabil"] + " yeets\nAdrian: " + yeetCount["Adrian"] + " yeets\nMitchell: " + yeetCount["Mitchell"] + " yeets\nDavid: " + yeetCount["David"] + " yeets\nNabil (the human one): :hitting_a_yeet: ");
     } else if(message.content.startsWith("Nabil, engage banning sequence on")) {
         var member = message.mentions.members.first();
-
+        message.channel.send("starting up. member = " + member);
         if(member == null) {
-            message.reply("Since you specified no one, you are going to be banned in " + parseInt(Math.random() * 30 + 30) + " seconds.");
+            message.channel.send("Since you specified no one, you are going to be banned in " + parseInt(Math.random() * 30 + 30) + " seconds.");
         } else if(member.user.bot) {
             message.reply("you thot.");
         } else {
